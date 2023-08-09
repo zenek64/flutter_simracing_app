fb8354b2cb4f73c0657fb12e8e09192dd082a4db
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-08-09 18:00:26 +0200

    initial_commit

---
+++ lib/fuel_calculator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class FuelCalculator extends StatefulWidget {
+  const FuelCalculator({super.key});
+
+  @override
+  State<FuelCalculator> createState() {
+    return __FuelCalculatorState();
+  }
+}
+
+class __FuelCalculatorState extends State<FuelCalculator> {
+  @override
+  Widget build(context) {
+    return const MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simracing_app/fuel_calculator.dart';
+
+void main() {
+  runApp(FuelCalculator());
+}
