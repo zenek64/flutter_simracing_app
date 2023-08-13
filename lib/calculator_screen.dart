748687fbc7cdb8e4d0686fca125ae6747c1b78b8
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-08-13 18:20:12 +0200

    text_fields

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 
-class CalculatorScreen extends StatelessWidget {
+class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  var myController = TextEditingController();
 
   @override
   Widget build(context) {
     return Container(
-        padding: const EdgeInsets.all(40),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: "Enter your number"),
-              keyboardType: TextInputType.number,
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          TextField(
+            decoration: const InputDecoration(labelText: "Enter your number"),
+            keyboardType: TextInputType.number,
+            controller: myController,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Container(
+              width: 100,
+              height: 70,
+              child: TextField(
+                decoration:
+                    const InputDecoration(labelText: "Enter your number"),
+                keyboardType: TextInputType.number,
+                controller: myController,
+              ),
             ),
-          ],
-        ));
+            Container(
+              width: 100,
+              height: 70,
+              child: TextField(
+                decoration:
+                    const InputDecoration(labelText: "Enter your number"),
+                keyboardType: TextInputType.number,
+                controller: myController,
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 70,
+              child: TextField(
+                decoration:
+                    const InputDecoration(labelText: "Enter your number"),
+                keyboardType: TextInputType.number,
+                controller: myController,
+              ),
+            )
+          ]),
+          TextField(
+            decoration: const InputDecoration(labelText: "Enter your number"),
+            keyboardType: TextInputType.number,
+            controller: myController,
+          )
+        ],
+      ),
+    );
   }
 }
