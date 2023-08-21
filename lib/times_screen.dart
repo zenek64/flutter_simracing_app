cc009195bb25b93ec70ac9ce0b14cc41cd219b10
Grzegorz <46374229+zenek64@users.noreply.github.com>
2023-08-21 17:28:43 +0200

    Create

--- lib/times_screen.dart
+++ lib/times_screen.dart
@@ -1,9 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'color_schemes.g.dart';
-import "package:cloud_firestore/cloud_firestore.dart";
 import 'dart:async';
 
 class Times extends StatefulWidget {
@@ -22,29 +18,83 @@ class _TimesState extends State<Times> {
   final CollectionReference _lapTimes =
       FirebaseFirestore.instance.collection('tblLapTimes');
 
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _carName,
+                  decoration: const InputDecoration(labelText: 'Car'),
+                ),
+                TextField(
+                  controller: _trackName,
+                  decoration: const InputDecoration(
+                    labelText: 'Track',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  onPressed: () async {
+                    final String name = _carName.text;
+                    final String price = _trackName.text;
+
+                    await _lapTimes.add({"CarName": name, "TrackName": price});
+
+                    _carName.text = '';
+                    _trackName.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: _lapTimes.snapshots(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapShot) {
-        if (streamSnapShot.hasData) {
-          return ListView.builder(
-              itemCount: streamSnapShot.data!.docs.length,
-              itemBuilder: (context, index) {
-                final DocumentSnapshot documentSnapshot =
-                    streamSnapShot.data!.docs[index];
-                return Card(
-                    margin: const EdgeInsets.all(16),
-                    child: ListTile(
-                      title: Text(documentSnapshot['CarName']),
-                      subtitle: Text(documentSnapshot['TrackName']),
-                    ));
-              });
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+    return Column(
+      children: [
+        Flexible(
+          child: StreamBuilder(
+            stream: _lapTimes.snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapShot) {
+              if (streamSnapShot.hasData) {
+                return ListView.builder(
+                    itemCount: streamSnapShot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      final DocumentSnapshot documentSnapshot =
+                          streamSnapShot.data!.docs[index];
+                      return Card(
+                          margin: const EdgeInsets.all(16),
+                          child: ListTile(
+                            title: Text(documentSnapshot['CarName']),
+                            subtitle: Text(documentSnapshot['TrackName']),
+                          ));
+                    });
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ),
+        TextButton(onPressed: _create, child: const Text("Add new time"))
+      ],
     );
   }
 }
